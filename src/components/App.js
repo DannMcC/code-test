558fa042f3aa5e7ae36c9bf0d42d723711a99fda
Dan McCune <megadannn@gmail.com>
2017-05-29 23:35:10 -0400

    implementing phone # check

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,21 @@ class App extends Component {
     super(props)
     this.state = {
       email: '',
-      emailCheck: 'Please enter an email'
+      emailCheck: 'Please enter an email',
+      phone: '',
+      phoneCheck: 'Please enter a phone number'
     }
 
     this.handleChange = this.handleChange.bind(this)
+    this.handleChangePhone = this.handleChangePhone.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange (event) {
     this.setState({email: event.target.value})
+  }
+  handleChangePhone (event) {
+    this.setState({phone: event.target.value})
   }
   emailChecker (x) {
     let foo = 'nothing'
@@ -26,10 +32,28 @@ class App extends Component {
     console.log(x)
     console.log(foo)
   }
+  phoneChecker (x) {
+    let foo = 'nothing'
+    let arr = x.split('')
+    if (
+      (arr[0] || arr[3] === 0) ||
+      (arr.length !== 10) ||
+      ((arr[0] - arr[1] === 0) && (arr[1] - arr[2] === 0)) ||
+      (x = '1234567890')
+
+    ) {
+      foo = 'ayyyyyy'
+    } else {
+      foo = 'naahhh'
+    }
+    console.log(x)
+    console.log(foo)
+  }
 
   handleSubmit (event) {
-    console.log('A name was submitted: ' + this.state.email)
+    console.log('Information has been submitted: ' + this.state.email + this.state.phone)
     this.emailChecker(this.state.email)
+    this.emailChecker(this.state.phone)
     event.preventDefault()
   }
   render () {
@@ -44,11 +68,20 @@ class App extends Component {
             this.handleChange
           }
          />
+        <p>{this.state.emailCheck}</p>
+        <input type='text'
+          value={
+              this.state.phone
+            }
+          onChange={
+              this.handleChangePhone
+            }
+           />
+        <p>{this.state.phoneCheck}</p>
         <input type='submit'
           value='Submit'
-          />
+            />
       </form>
-      <p>{this.state.emailCheck}</p>
     </div>
   }
 }
